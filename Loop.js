9293ccfccff382c138c1e07fe1d75a36e311100c
khalid <khleadini@gmail.com>
2022-05-30 16:29:16 +0300

    Loop Challenges

---
+++ Loop.js
@@ -0,0 +1,39 @@
+//----Print odds 1-20--------//
+
+// for(var i = 1; i<20; i+=2){
+//     console.log(i)
+// }
+
+
+//----Decreasing Multiples of 3--------//
+
+// for(var i =100; i>-1 ; i--){
+//     if(i%3 ==0){
+//        console.log(i)
+//     }
+// }
+
+
+//----Print the sequence--------//
+
+// for(var i=4;i>-5;i-=1.5){
+//     console.log(i)
+// }
+
+
+//----Sigma--------//
+
+// var x=0 
+// for(var i=1; i<101; i++){
+//     x += i;
+// }
+// console.log(x)
+
+
+//----Factorial--------//
+
+// var x=1 
+// for(var i=1; i<13; i++){
+//     x *= i;
+// }
+// console.log(x)
